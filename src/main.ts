0ed476a93a16345b665f1c1d047a25dc77d58eb6
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-05 15:03:57 +0200

    add and configure app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { apollo } from './Apollo'
+import { gqlURI } from './config'
+import { createApp, provide, h } from 'vue'
 
-createApp(App).mount('#app')
+apollo.init({ getUrl: gqlURI })
+const app = createApp({
+  setup() {
+    provide('ApolloClient', apollo.get())
+  },
+  render() {
+    return h(App)
+  }
+})
+app.use(router)
+app.mount('#app')
