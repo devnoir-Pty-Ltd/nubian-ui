717f0b03ea46698e1d954fcbdc17a2e8a7694cf3
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-05 15:01:48 +0200

    add and configure layouts

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="nubian-app">
+    <AppHeader />
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex-container">
+    <div class="logo-wrapper"></div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
