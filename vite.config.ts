9c4cb9de8a9eac7240d7451ab1fd7e8af612c15b
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-05 10:51:06 +0200

    basic project setup updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
+import vue from '@vitejs/plugin-vue';
+const pathSrc = path.resolve(__dirname, './src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [vue(), eslintPlugin()],
+	server: {
+		port: 3001,
+	},
+	css: {
+		preprocessorOptions: {
+			css: { additionalData: `@import "${pathSrc}/assets/css/index.css";` },
+		},
+	},
+});
